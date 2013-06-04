7eb22aa5beb709d834f39d0a0dfb453c2e68b773
denisovdenis <denisov@undev.ru>
2013-06-04 14:13:47 +0400

    Changed hook

---
+++ InlineDiff.i18n.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Internationalisation file for extension InlineDiff.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+$messages['ru'] = array(
+	'emailsubject' => 'Изменения затрагивающие ',
+    'enotif_body' => 'Здравствуйте, $WATCHINGUSERNAME,<br><br>
+
+$TIMESTAMP участником $PAGEEDITOR_WIKI была изменена страница &laquo;$PAGE&raquo; проекта &laquo;$SITENAME&raquo; с именем.<br><br>
+
+$DIFF<br><br>
+
+Вы можете ознакомиться с $NEWPAGE изменениями, а также со всеми $ALLPAGECHANGES.',
+);
